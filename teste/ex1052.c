93bd6c1236479f1ab9c20154380e4c1772b7e2a0
bill-gates-da-ufu <95662915+bill-gates-da-ufu@users.noreply.github.com>
2021-12-06 22:02:25 -0300

    Adicionando o exercício 1052

---
+++ teste/ex1052.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main() {
+    int monthNumber;
+    scanf("%d", &monthNumber);
+    if (monthNumber == 1) {
+        printf("January\n");
+    }
+    if (monthNumber == 2) {
+        printf("February\n");
+    }
+    if (monthNumber == 3) {
+        printf("March\n");
+    }
+    if (monthNumber == 4) {
+        printf("April\n");
+    }
+    if (monthNumber == 5) {
+        printf("March\n");
+    }
+    if (monthNumber == 6) {
+        printf("March\n");
+    }
+    if (monthNumber == 7) {
+        printf("June\n");
+    }
+    if (monthNumber == 8) {
+        printf("July\n");
+    }
+    if (monthNumber == 9) {
+        printf("August\n");
+    }
+    if (monthNumber == 10) {
+        printf("October\n");
+    }
+    if (monthNumber == 11) {
+        printf("November\n");
+    }
+    if (monthNumber == 11) {
+        printf("December\n");
+    }
+    
+    return 0;   
+}
